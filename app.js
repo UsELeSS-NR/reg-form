1249032f81b74879a203e0528b6ba7d7a5f627e1
UsELeSS-NR <110571067+UsELeSS-NR@users.noreply.github.com>
2022-08-30 03:14:48 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+
+$ (document).ready(function(){
+    document.getElementById("fname_res").innerHTML=localStorage.getItem("textValue1");
+    document.getElementById("mname_res").innerHTML=localStorage.getItem("textValue2");
+    document.getElementById("lname_res").innerHTML=localStorage.getItem("textValue3");
+    document.getElementById("dob_res").innerHTML=localStorage.getItem("textValue4");
+    document.getElementById("gender_res").innerHTML=localStorage.getItem("textValue5");
+    document.getElementById("add1_res").innerHTML=localStorage.getItem("textValue6");
+    document.getElementById("add2_res").innerHTML=localStorage.getItem("textValue7");
+    document.getElementById("city_res").innerHTML=localStorage.getItem("textValue8");
+    document.getElementById("state_res").innerHTML=localStorage.getItem("textValue9");
+    document.getElementById("code_res").innerHTML=localStorage.getItem("textValue10");
+    document.getElementById("mail_res").innerHTML=localStorage.getItem("textValue11");
+    document.getElementById("contact_res").innerHTML=localStorage.getItem("textValue12");
+    document.getElementById("comment_res").innerHTML=localStorage.getItem("textValue13");
+
+
+//     $ ("#btn_print").click(function(){
+//         $ ("#btn_print").css("color", "red");
+    
+// })
+
+})
